1a985a5d2327bf7fbbb2c0ef077c40717c51df5d
drbenasmah <drbenasmah2016@gmail.com>
2023-09-26 00:13:52 +0200

    Task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,10 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    let innerTask = true;
+    let innerTask2 = false;
+    task = innerTask;
+    task2 = innerTask2;
   }
 
   return [task, task2];
